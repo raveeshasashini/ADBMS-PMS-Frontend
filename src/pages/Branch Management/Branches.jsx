7d708d51248205967ae9faf5a48bc3b4ed9f9e73
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-11-02 22:18:42 +0530

    Update Branches.jsx

--- src/pages/Branch Management/Branches.jsx
+++ src/pages/Branch Management/Branches.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Branches() {
   return (
-    <div style={{marginTop:'40px'}}>Hello</div>
+    <div className="app-container">
+    </div>
   )
 }
