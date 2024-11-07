3a69914c6c18ad720aa2a9778fb3dea4cad38226
Maneth Hasindu <manethhasidu@gmail.com>
2024-11-07 14:15:12 +0530

    Update stock.jsx

--- src/pages/StockManagement/stock.jsx
+++ src/pages/StockManagement/stock.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './stock.css';
 
-
-
 function Stock() {
+  
+
   return (
     <div className="stock-table">
       <h3>All Stock Details</h3>
       <button className="add-stock">Remove Stock</button>
-      <button className="add-stock">Change sale price</button>
+      <button className="add-stock">Change Sale Price</button>
       <table>
         <thead>
           <tr>
             <th>Medicine ID</th>
             <th>Medicine Name</th>
-            <th>Supplier Details</th>
-            <th>Unit Type</th>
-            <th>Dose</th>
+            <th>Stock Quantity</th>
+            <th>Unit Price</th>
+            <th>Branch Details</th>
             <th>Actions</th>
           </tr>
         </thead>
+    
       </table>
     </div>
   );
